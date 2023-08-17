62b78c045e148f51173f0d75a90b37d651d1ccd0
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-08-17 12:01:42 +0500

    Added project files

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Abdullah Mujahid',
+    default:
+      'Abdullah Mujahid - Software engineer, and Project manager',
+  },
+  description:
+    'I’m Abdullah, a software engineer and a project manager based in Lahore, Pakistan.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
