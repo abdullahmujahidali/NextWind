3cf64724040760c8aace4926baddd133c223708a
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-08-17 22:21:31 +0500

    fixed landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -175,7 +175,7 @@ function Resume() {
     {
       company: 'deSwot',
       title: 'Full Stack Software Engineer',
-      logo: logoStarbucks,
+      logo: logoFacebook,
       start: '2020',
       end: '2021',
     },
@@ -264,39 +264,45 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Senior Software engineer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Abdullah Mujahid, an experienced full-stack software developer
+            from Lahore, Pakistan. With 5 years experience, I excel in crafting
+            advanced solutions for diverse industries. My expertise spans Web
+            and AI, navigating stacks like Django, React JS, Node JS, and JAVA.
+            I’m adept with AWS and creating user-friendly interfaces for
+            exceptional UX. Staying current with emerging tech, I'm a reliable
+            and informed developer. I enjoy solving problems, adapting fast, and
+            delivering tailored top-tier solutions. Passionate about innovation
+            and collaboration, I'm eager to contribute to dynamic environments
+            and growth-focused projects. My blockchain familiarity and project
+            history enrich my capabilities, adding depth to my versatility.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
+              href="https://twitter.com/abdulladgaf"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
             <SocialLink
-              href="https://instagram.com"
+              href="https://www.instagram.com/abdullahmujahidali/"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/abdullahmujahidali"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/abdullah-mujahid-211849186/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
           </div>
         </div>
       </Container>
-      <Photos />
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
