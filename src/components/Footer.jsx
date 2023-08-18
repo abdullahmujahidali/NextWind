6a0f26f94dea2c653493ecd57c6ae577152756d7
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-08-18 10:30:01 +0500

    fixed ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,11 +23,11 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
+                {/* <NavLink href="/speaking">Speaking</NavLink> */}
                 <NavLink href="/uses">Uses</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Spencer Sharp. All rights
+                &copy; {new Date().getFullYear()} Abdullah Mujahid. All rights
                 reserved.
               </p>
             </div>
