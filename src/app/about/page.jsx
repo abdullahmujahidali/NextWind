824479c622998376bdd266c6a6f06d11f0d29ccc
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-26 16:35:29 +0500

    updated about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -63,31 +63,38 @@ export default function About() {
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              As a child, my love for technology was evident. I immersed myself
+              in the world of computers, tinkering with code and exploring the
+              endless possibilities of programming languages. With a keen
+              interest in AI, I delved into research, culminating in the
+              publication of a paper on Hand Gesture Recognition, showcasing my
+              early passion and expertise.
+              <a href="https://www.mdpi.com/2076-3417/11/9/4164">
+                My Published Paper
+              </a>
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              Fast forward to my career as a Senior Full Stack Software
+              Engineer, I find myself at the forefront of innovation. From
+              spearheading comprehensive refactoring efforts to optimizing
+              backend processes and enhancing frontend usability, each project
+              is a testament to my dedication and proficiency in utilizing
+              technologies like Django and React to drive impactful solutions.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              Whether it's developing essential backend APIs for NFT
+              marketplaces or revamping existing systems for enhanced
+              efficiency, my journey in software engineering is characterized by
+              a relentless pursuit of excellence. With each line of code, I'm
+              committed to pushing the boundaries of what's possible, shaping
+              the digital landscape one project at a time.
             </p>
-            <p>
+            {/* <p>
               Today, I’m the founder of Planetaria, where we’re working on
               civilian space suits and manned shuttle kits you can assemble at
               home so that the next generation of kids really <em>can</em> make
               it to orbit — from the comfort of their own backyards.
-            </p>
+            </p> */}
           </div>
         </div>
         <div className="lg:pl-20">
