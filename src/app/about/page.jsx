99009c21c4f3c24a60038b976e05a67ca299ec46
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-26 16:27:32 +0500

    updayed image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/abdullah.JPG'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
